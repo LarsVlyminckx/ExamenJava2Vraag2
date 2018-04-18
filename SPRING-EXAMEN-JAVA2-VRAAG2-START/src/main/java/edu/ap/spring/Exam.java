69d976e0c7bca4daa24f82174e384407841fac1e
LarsVlyminckx <lars.vlyminckx@hotmail.com>
2018-04-18 17:31:52 +0200

    toevoegen screenshots

--- SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam.java
+++ SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam.java
@@ -32,11 +32,7 @@ public class Exam {
 	// x-coordinaten uit de lijst van points te berekenen
 	// 1 punt
 	public int sumOfX(List<Point> points) {
-		int count = 0;
-		for (Point point : points) {
-			count+= point.getX();
-		}
-		return count;
+		return (points.stream().filter(x -> x.getX()).count());
 	}
 	
 	// Maak gebruik van lambdas en streams om een comma-separated
@@ -44,12 +40,11 @@ public class Exam {
 	// zijn aan twee
 	// 2 punten
 	public String getXOverTwo(List<Point> points) {
+		
 		String result = "";
-		for (Point point : points) {
-			if (point.getX() >= 2) {
-				result += point.getX()+ ", ";
-			}
-		}
+		
+		result = points.stream().filter(x -> x.getX() >= 2).toString();
+		result = result.concat(", ");
 		return result;
 		
 	}
